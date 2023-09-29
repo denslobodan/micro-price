b874099943b3019a7a5c4880b3f4ccc123d390c7
Knight of belief <slbdenis.victor@gmail.com>
2023-09-29 19:52:29 +0300

    Added logic with loggining

--- service.go
+++ service.go
@@ -4,33 +4,48 @@ import (
 	"context"
 	"fmt"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
+
+var prices = map[string]float64{
+	"BTC": 20_000.0,
+	"ETH": 999.99,
+	"GG":  100_000.0,
+}
 
 // PriceFetcher is an interface that can fetch a price.
 type PriceFetcher interface {
 	FetchPrice(context.Context, string) (float64, error)
 }
 
-// priceFetcher implements the PriceFfetcher interface.
-type priceFetch struct{}
+// priceService implements the PriceFfetcher interface.
+type priceService struct{}
 
-func (s *priceFetch) FetchPrice(ctx context.Context, ticker string) (float64, error) {
-	return MockPriceFetcher(ctx, ticker)
-}
-
-var priceMocks = map[string]float64{
-	"BTC": 20_000.0,
-	"ETH": 200.0,
-	"GG":  100_000.0,
-}
-
-func MockPriceFetcher(ctx context.Context, ticker string) (float64, error) {
-	// mimic the HTTP roundtrip
-	time.Sleep(100 * time.Millisecond)
-
-	price, ok := priceMocks[ticker]
+func (s *priceService) FetchPrice(_ context.Context, ticker string) (float64, error) {
+	price, ok := prices[ticker]
 	if !ok {
-		return price, fmt.Errorf("the given ticker (%s) in not supported", ticker)
+		return 0.0, fmt.Errorf("price for ticker (%s) is not available", ticker)
 	}
 	return price, nil
+}
+
+type logginingService struct {
+	priceService
+}
+
+func (s logginingService) FetchPrice(ctx context.Context, ticker string) (price float64, err error) {
+	defer func(begin time.Time) {
+		reqID := ctx.Value("requestID")
+
+		logrus.WithFields(logrus.Fields{
+			"requestID": reqID,
+			"took":      time.Since(begin),
+			"err":       err,
+			"price":     price,
+			"ticker":    ticker,
+		}).Info("FetchPrice")
+	}(time.Now())
+
+	return s.priceService.FetchPrice(ctx, ticker)
 }
