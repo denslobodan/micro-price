ae94c4cf34f417068084e10719ae7151b5628bbf
Knight of belief <slbdenis.victor@gmail.com>
2023-10-01 10:34:31 +0300

    .

--- metrics.go
+++ metrics.go
@@ -14,8 +14,3 @@ func (s *MetricService) FetchPrice(ctx context.Context, ticker string) (price fl
 	//  your metrcis storage. Push to prometheus (gauge, counters)
 	return s.next.FetchPrice(ctx, ticker)
 }
-
-// 1. Найти созданную в goose базу данных
-// 2. Зачем открывать в каждом методе соединение с базой
-// 3. Что такое формат hcl и как с ним работать
-// 4. Вычищение html тегов
